6e498ecc5021861b6f307b6aea1424f754f5718b
Owen <owenrees82@gmail.com>
2024-12-04 13:06:05 +0100

    runtime env var

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,3 @@ if config_env() in [:dev, :test] do
     if File.exists?(path), do: import_config(path)
   end
 end
-
-config :file_server,
-  port: 4321,
-  fileserver_auth_key: System.get_env("FILESERVER_AUTH_KEY")
