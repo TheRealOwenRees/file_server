9aaf30c49f6e7fffab26ef08d5eb5ee40e20fdb3
Owen <owenrees82@gmail.com>
2024-12-04 13:20:58 +0100

    runtime env var

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,12 @@
-# import Config
+import Config
 
-# if config_env() in [:dev, :test] do
-#   for path <- [".env.exs", ".env.#{config_env()}.exs"] do
-#     path = Path.join(__DIR__, "..") |> Path.join("config") |> Path.join(path) |> Path.expand()
-#     if File.exists?(path), do: import_config(path)
-#   end
-# end
+if config_env() in [:prod, :dev, :test] do
+  for path <- [".env.exs", ".env.#{config_env()}.exs"] do
+    path = Path.join(__DIR__, "..") |> Path.join("config") |> Path.join(path) |> Path.expand()
+    if File.exists?(path), do: import_config(path)
+  end
+end
 
-# config :file_server,
-#   port: 4321,
-#   fileserver_auth_key: nil
+config :file_server,
+  port: 4321,
+  fileserver_auth_key: System.get_env("FILESERVER_AUTH_KEY")
